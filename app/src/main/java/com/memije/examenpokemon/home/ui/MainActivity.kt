ee5736c1f9d2deab93cb37138151e4468c1fc0ba
Jesús González Memije <memije.dev@gmail.com>
2022-06-10 13:39:21 -0500

    Initial commit

---
+++ app/src/main/java/com/memije/examenpokemon/home/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.memije.examenpokemon.home.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.memije.examenpokemon.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
